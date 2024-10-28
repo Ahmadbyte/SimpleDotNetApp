ae750032fa805ec7b662230ffd7f7c00ced3bd52
Ahmadbyte <shariqueahmad011@gmail.com>
2024-10-28 23:51:12 +0530

    new

--- HelloWebApp/obj/Debug/net8.0/HelloWebApp.AssemblyInfo.cs
+++ HelloWebApp/obj/Debug/net8.0/HelloWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2319eac5654ca9ebc63497e3b32382bd6a59e549")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
